af7e942a6aad9a8ab74ab1c570131084107f4131
tybt-123 <86039554+tybt-123@users.noreply.github.com>
2021-06-25 17:26:13 +0800

    Updated and renamed file

--- upgrade.php
+++ upgrade.php
@@ -37,7 +37,7 @@ if(isset($_POST['databaseType'])){
     if(!empty($diff)){
         __pr($diff);
         ## sample: 
-        ## exec('/mycro/install_rds.sh');
+        exec('./dbMigrate.sh');
         header("Location: https://console.aws.amazon.com/cloudformation/home?#/stacks/new?stackName=add_RDS&templateURL=https://quick-launch-ecomm.s3.us-east-2.amazonaws.com/RDStemplate.yaml");
         
     }
